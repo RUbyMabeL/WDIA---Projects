c59052d37d2b52da29fa10cac661c6b791037f88
Xiaojun Ma <113055086+RUbyMabeL@users.noreply.github.com>
2024-08-22 14:59:31 -0400

    Add files via upload

---
+++ C# - Build and Use an API/Lab6Client/Controllers/HomeController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lab6Client.Models;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Net;
+
+namespace Lab6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _serviceUrl;
+
+        public HomeController(IConfiguration config)
+        {
+            _configuration = config;
+            _serviceUrl = _configuration.GetValue<string>("ServerURL");
+        }
+
+        // GET: Home/Index
+        public async Task<IActionResult> Index()
+        {
+            List<RestaurantInfo> restaurants = new List<RestaurantInfo>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync($"{_serviceUrl}/RestaurantReview");
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    restaurants = JsonSerializer.Deserialize<List<RestaurantInfo>>(data);
+                }
+            }
+
+            return View(restaurants);
+        }
+
+        // GET: Home/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            RestaurantInfo restaurant = null;
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync($"{_serviceUrl}/RestaurantReview/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    restaurant = JsonSerializer.Deserialize<RestaurantInfo>(data);
+                }
+            }
+
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return View(restaurant);
+        }
+
+        // POST: Home/Edit
+        [HttpPost]
+        public async Task<IActionResult> Edit(RestaurantInfo restInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(restInfo);
+            }
+
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new StringContent(JsonSerializer.Serialize(restInfo), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync($"{_serviceUrl}/RestaurantReview", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(restInfo);
+        }
+
+        // GET: Home/New
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        // POST: Home/New
+        [HttpPost]
+        public async Task<IActionResult> New(RestaurantInfo restInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(restInfo);
+            }
+
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new StringContent(JsonSerializer.Serialize(restInfo), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync($"{_serviceUrl}/RestaurantReview", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(restInfo);
+        }
+
+        // GET: Home/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.DeleteAsync($"{_serviceUrl}/RestaurantReview/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return NotFound();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ C# - Build and Use an API/Lab6Client/Models/RestaurantInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab6Client.Models
+{
+    public class Address
+    {
+        public string street { get; set; }
+        public string city { get; set; }
+        [RegularExpression(@"^(ON|BC|QC|MB|SK|AB|NB|NS|PE|NL|NT|YT|NU)$", ErrorMessage = "Province must be one of the Canadian provinces or territories (e.g., ON, BC, QC, etc.)")]
+        public string provstate { get; set; }
+        [RegularExpression(@"^[A-Z]\d[A-Z] \d[A-Z]\d$", ErrorMessage = "Postal code must be in the format L2L 3K4")]
+        public string postalzipcode { get; set; }
+    }
+
+    public class Rating
+    {
+        public int minRating { get; set; } = 0;         //default to 0
+        public int maxRating { get; set; } = 5;         //default to 5
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int currentRating { get; set; }
+    }
+    public class Cost
+    {
+        public int minCost { get; set; } = 0;
+        public int maxCost { get; set; } = 5;
+        [Range(1, 5, ErrorMessage = "Cost must be between 1 and 5.")]
+        public int currentCost { get; set; }
+    }
+    public class RestaurantInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public Address address { get; set; }
+        public string summary { get; set; }
+        public string foodType { get; set; }
+        public Rating rating { get; set; }
+
+        public Cost cost { get; set; }
+
+    }
+}
